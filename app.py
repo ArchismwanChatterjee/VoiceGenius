152bc0ef393ed5d9b2d3a90eb7570ac9365bcfd9
Archismwan Chatterjee <115975340+ArchismwanChatterjee@users.noreply.github.com>
2024-02-25 23:47:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import speech_recognition as sr
 from gtts import gTTS
 import google.generativeai as genai
 import IPython.display as ipd
+import time
+# from pydub import AudioSegment
+from mutagen.mp3 import MP3
 
 genai.configure(api_key="AIzaSyAbVgMWasN83pawqw02-iQHcEJqPQkDl2Y")
 
@@ -40,14 +43,21 @@ def detect(text):
     prompt_parts = [
         f'''Answer: {text}''', ]
     response = model.generate_content(prompt_parts)
+    
     tts = gTTS(text=response.text, lang='en')
     st.write(response.text)
     tts.save("output.mp3")
+
+    audio = MP3("output.mp3")
+    audio_duration = audio.info.length
+
     audio_player = ipd.Audio("output.mp3", autoplay=True)
     st.write(audio_player)
+    time.sleep(audio_duration)
 
 
 def perform_action(command):
+    
     if "what is" in command:
         detect(command)
     else:
@@ -55,22 +65,46 @@ def perform_action(command):
 
 
 if __name__ == "__main__":
+    
     st.title("Voice-enabled Streamlit App")
-
-    stop_button = st.button("Stop")
-    continue_button = st.button("Continue")
+    st.info("Start the question by saying What is <your question>")
+    stop_button = st.button("STOP")
+    continue_button = st.button("ASK")
+    
+    c=0
 
     while continue_button:
+        
         if stop_button:
             st.warning("User clicked on Stop. Stopping the continuous listening.")
             break
 
         user_command = recognize_speech()
-
+        
+        c+=1
+        
         if user_command:
             perform_action(user_command)
 
-        # Sleep to avoid high CPU usage
-        # st.experimental_rerun()
+        st.write("Listening for next command...")
+        time.sleep(3)
 
-    st.write("Continuous listening stopped.")
+        if c==5:
+            break
+
+    if c==5:
+        st.write("Limit of 5 chat at a stretch is reached restart the app to continue.")
+        
+        if st.button('Rerun App'):
+            st.experimental_rerun()
+    else:
+        st.write("Click on ASK to start and if you want to stop anytime then click on STOP.")
+
+
+'''
+    audio = AudioSegment.from_file("output.mp3")
+    audio_duration = len(audio) / 1000  # Get duration in seconds
+
+    If you want to use Pydub module then the above code can be used to get the duration of the audio file.
+
+'''
